2cee42e27948ab5e1a4c374efa13ad068084315c
Nirmal-Seervi <128449025+Nirmal-Seervi@users.noreply.github.com>
2024-10-05 23:54:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Not a subset sum/not-a-subset-sum.java
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.java
@@ -28,9 +28,10 @@ public class Main {
 
 class Solution {
     public long findSmallest(int[] arr) {
+        // Your code goes here
         long res = 1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > res) {
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > res){
                 return res;
             }
             res += arr[i];
