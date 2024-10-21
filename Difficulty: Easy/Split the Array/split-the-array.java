be2a5d19f15fbb06ef78f1d44c75cd96c0913061
NimCraft <128449025+Nirmal-Seervi@users.noreply.github.com>
2024-10-21 23:56:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split the Array/split-the-array.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    
+    static int mod=1000000007;
+    
+    static int solve(int[] arr,int idx,int hxor,int txor){
+        
+        if(idx==arr.length){
+            if((hxor^txor)==0){
+                return 1;
+            }
+        }
+        
+        return (solve(arr,idx+1,hxor^arr[idx],txor)%mod+
+                solve(arr,idx+1,hxor,txor)%mod)%mod;
+        
+    }
+
+    public static int countgroup(int arr[]) {
+        
+          int xor = 0;
+        int n = arr.length;
+        int mod = 1000000007;
+        for(int i = 0; i<n; i++){
+            xor ^= arr[i];
+        }
+        if(xor != 0) return 0;
+        return (int)(Math.pow(2, n-1) - 1)%mod;
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int ans = obj.countgroup(arr);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
